934243e799c0d7328bac5abdea07a42da10c9c28
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-15 15:19:52 -0500

    initial commit

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,8 +3,8 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -16,11 +16,10 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'FRANK')
-
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -32,7 +31,77 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
-
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present but written in lowercase", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'seinfeld')
+    assert.isTrue(result);
+  });
+  
+  it("should return false for an empty word", function () {
+    const result = wordSearch([
+      ['A', 'B', 'C'],
+      ['D', 'E', 'F'],
+      ['G', 'H', 'I'],
+    ], '');
+    assert.isFalse(result);
+  });
+
+  it("should return false if the word is longer than any row or column", function () {
+    const result = wordSearch([
+      ['A', 'B', 'C'],
+      ['D', 'E', 'F'],
+      ['G', 'H', 'I'],
+    ], 'ABCDEFGHI');
+    assert.isFalse(result);
+  });
+
+  it("should return false if input is not a string", function () {
+    const result = wordSearch([
+      ['A', 'B', 'C'],
+      ['D', 'E', 'F'],
+      ['G', 'H', 'I'],
+    ], 123);
+    assert.isFalse(result);
+  });
+
+  it("should return true if letter is found in any row or column", function () {
+    const result = wordSearch([
+      ['A', 'B', 'C'],
+      ['D', 'E', 'F'],
+      ['G', 'H', 'I'],
+    ], `E`);
+    assert.isTrue(result);
+  });
+
+  it("should return false if letter is not found in any row or column", function () {
+    const result = wordSearch([
+      ['A', 'B', 'C'],
+      ['D', 'E', 'F'],
+      ['G', 'H', 'I'],
+    ], `Z`);
+    assert.isFalse(result);
+  });
+
+  it("should return true if letter is found in 1 letter matrix", function () {
+    const result = wordSearch([
+      [`A`],
+      ['B'],
+      ['C'],
+      ['D']
+    ], `A`);
+    assert.isTrue(result);
+  })
 });
+
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+        
+  if (word === "" || letters.length === 0 || letters[0].length === 0 || typeof word !== `string`) {
+    return false;
+  }
+    
+  word = word.toUpperCase();
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word))
+      return true;
+  }
+  
+  for (let i = 0; i < letters[0].length; i++) {
+    const verticalJoin = letters.map(ls => ls[i]).join('');
+    if (verticalJoin.includes(word))
+      return true;
+  }
+
+  return false;
+};
+
+
+module.exports = wordSearch;
